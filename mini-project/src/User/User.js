32dfc106969c192285e64e7c5ae82cfa35bf5415
Praful Shrestha <prafulshrestha.softace@outlook.com>
2022-04-10 22:27:03 +0545

    module 8 validations

--- mini-project/src/User/User.js
+++ mini-project/src/User/User.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import Card from './UI/Card'
+import styles from './User.module.css'
+import Button from './UI/Button'
 
 function User() {
     const [user, setUser] = useState({
@@ -9,26 +12,40 @@ function User() {
     function changeUser(e) {
         setUser(prev => {
             return {
-                [e.target.name]: e.target.value,
                 ...prev,
+                [e.target.name]: e.target.value,
             }
         })
     }
 
     function submitUser(e) {
         e.preventDefault();
+        if (user.age.trim().length === 0 || user.userName.trim().length === 0) {
+            console.log("length validation occured")
+            return;
+        }
+        if (+user.age < 1) {
+            console.log("less than 1 validation occured")
+            return;
+        }
         console.log(user)
+        setUser({
+            userName: "",
+            age: ""
+        })
     }
 
     return (
-        <form onSubmit={submitUser}>
-            <label htmlFor="userName">Username</label>
-            <input type="text" onChange={changeUser} name="userName" value={user.userName} />
-            <label htmlFor="age">Age(Years)</label>
-            <input type="text" onChange={changeUser} name="age" value={user.age} />
-            <button type='submit'>Add User</button>
-            <p>{`${user.userName} (${user.age} years old)`}</p>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={submitUser}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" onChange={changeUser} name="userName" value={user.userName} />
+                <label htmlFor="age">Age(Years)</label>
+                <input id="age" type="number" onChange={changeUser} name="age" value={user.age} />
+                <Button type='submit'>Add User</Button>
+                <p>{`${user.userName} (${user.age} years old)`}</p>
+            </form>
+        </Card>
     )
 }
 
