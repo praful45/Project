fd84335747505eb6fdc20ae0d3be3f7a913168cd
Praful Shrestha <prafulshrestha.softace@outlook.com>
2022-04-12 21:47:52 +0545

    module 8 - Error Module

--- mini-project/src/User/User.js
+++ mini-project/src/User/User.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import Card from './UI/Card'
 import styles from './User.module.css'
 import Button from './UI/Button'
+import ErrorModal from './UI/ErrorModal'
 
 function User(props) {
     const [user, setUser] = useState({
         id: "",
         userName: "",
         age: ""
+    })
+
+    const [error, setError] = useState({
+        title: "",
+        message: "",
+        isError: false
     })
 
     function changeUser(e) {
@@ -23,10 +30,20 @@ function User(props) {
     function submitUser(e) {
         e.preventDefault();
         if (user.age.trim().length === 0 || user.userName.trim().length === 0) {
+            setError({
+                title: "Length Validation",
+                message: "Name or Age should not be empty",
+                isError: true
+            })
             console.log("length validation occured")
             return;
         }
         if (+user.age < 1) {
+            setError({
+                title: "age Validation",
+                message: "Age is less than 1",
+                isError: true
+            })
             console.log("less than 1 validation occured")
             return;
         }
@@ -37,16 +54,27 @@ function User(props) {
         })
     }
 
+    function resolveError() {
+        setError({
+            title: "",
+            message: "",
+            isError: false
+        })
+    }
+
     return (
-        <Card className={styles.input}>
-            <form onSubmit={submitUser}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={changeUser} name="userName" value={user.userName} />
-                <label htmlFor="age">Age(Years)</label>
-                <input id="age" type="number" onChange={changeUser} name="age" value={user.age} />
-                <Button type='submit'>Add User</Button>
-            </form>
-        </Card>
+        <>
+            {error.isError && <ErrorModal title={error.title} message={error.message} onConfirm={resolveError} />}
+            <Card className={styles.input}>
+                <form onSubmit={submitUser}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" onChange={changeUser} name="userName" value={user.userName} />
+                    <label htmlFor="age">Age(Years)</label>
+                    <input id="age" type="number" onChange={changeUser} name="age" value={user.age} />
+                    <Button type='submit'>Add User</Button>
+                </form>
+            </Card>
+        </>
     )
 }
 
