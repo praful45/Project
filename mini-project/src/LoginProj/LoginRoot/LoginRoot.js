29e62d2ce9ade4b1f9a3e9404e77b1bcfd6857a1
Praful Shrestha <prafulshrestha.softace@outlook.com>
2022-04-23 22:49:27 +0545

    module 10 useeffect

--- mini-project/src/LoginProj/LoginRoot/LoginRoot.js
+++ mini-project/src/LoginProj/LoginRoot/LoginRoot.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from '../Home/Home'
 import Login from '../Login/Login'
 import MainHeader from '../MainHeader/MainHeader';
@@ -6,13 +6,22 @@ import MainHeader from '../MainHeader/MainHeader';
 function LoginRoot() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const loginHandler = (email, password) => {
-        if (email.isEmpty() || password.isEmpty()) {
-            console.log("not valid")
+    useEffect(() => {
+        const storedInfo = localStorage.getItem('isLoggedIn');
+        if (storedInfo === '1') {
+            setIsLoggedIn(true)
         }
+    }, [])
+
+    const loginHandler = (email, password) => {
+        // if (email.isEmpty() || password.isEmpty()) {
+        //     console.log("not valid")
+        // }
+        localStorage.setItem("isLoggedIn", "1")
         setIsLoggedIn(true)
     }
     const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
         setIsLoggedIn(false)
     }
 
